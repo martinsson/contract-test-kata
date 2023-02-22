2ccba67409eded83c2034df0c49f538f2dba0c4c
Johan Martinsson <jma@notsodark.com>
2023-02-22 01:31:14 +0100

    initiate php version

---
+++ simplemessage-client/php/tests/SimpleMessageContractTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleMessageContractTest extends TestCase
+{
+    /** @test */
+    public function list_received_messages()
+    {
+        $this->fail("interact with server http://localhost:8080/:userId/messages and user id 1");
+    }
+
+
+}
